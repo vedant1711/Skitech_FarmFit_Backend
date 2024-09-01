fc743d3542d2edf204e1746b14744f61605fa3ac
vedant1711 <vscode1711@gmail.com>
2024-09-01 09:57:18 +0530

    feat: Add CustomUser model and API

--- blogs/models.py
+++ blogs/models.py
@@ -1,11 +1,27 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.text import slugify
-
-from django.contrib.auth.models import AbstractUser
-from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError('The Phone Number field must be set')
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(phone_number, password, **extra_fields)
 
 class CustomUser(AbstractUser):
     username = None  # Remove the username field
@@ -20,9 +36,10 @@ class CustomUser(AbstractUser):
     USERNAME_FIELD = 'phone_number'
     REQUIRED_FIELDS = []  # No additional fields are required
 
+    objects = CustomUserManager()
+
     def __str__(self):
         return self.phone_number
-
 
 class BlogEntry(models.Model):
     user = models.ForeignKey(CustomUser, related_name='blogs', on_delete=models.CASCADE)
