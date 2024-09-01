e3c99ffd5b67d9201f1cdd444b0a0221976855fd
vedant1711 <vscode1711@gmail.com>
2024-09-01 17:12:21 +0530

    refactor: Update URLs for BlogEntry and CustomUserViewSet

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from blogs.views import BlogEntryViewSet, CustomUserViewSet
 
 router = DefaultRouter()
-router.register(r'blogentry/', BlogEntryViewSet, basename='blogentry')
-router.register(r'users/', CustomUserViewSet, basename='users')
+router.register(r'blogentry', BlogEntryViewSet, basename='blogentry')
+router.register(r'users', CustomUserViewSet, basename='users')
 urlpatterns = [
     path('', include(router.urls)),
 ]
